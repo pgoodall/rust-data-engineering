b234fe9fb2ed9f8796ccbddb893040ba3ee5eb4b
Pete Goodall <pete@digicratic.com>
2026-05-08 17:43:56 +0100

    WIP on hashmap-count exercise

---
+++ hashmap-count/src/lib.rs
@@ -0,0 +1,56 @@
+use clap::Parser;
+use std::collections::HashMap;
+use std::hash::Hash;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+pub struct Config {
+    /// A list of numbers with some numbers repeated
+    #[arg(short, long)]
+    pub num_list: Option<String>,
+
+    /// Count occurances of a work in a short sentence
+    #[arg(short, long)]
+    pub sentence: Option<String>,
+
+    /// Word for which to search in a supplied sentence or file
+    #[arg(short, long)]
+    pub word: Option<String>,
+
+    /// Search the given file for a given word
+    #[arg(short, long)]
+    pub file: Option<String>,
+}
+
+pub fn frequency<T>(content: Vec<T>) -> HashMap<T, i32> 
+where
+    T: Eq + PartialEq + Hash,
+{
+    let mut frequencies = HashMap::new();
+    for k in content {
+            let count = frequencies.entry(k).or_insert(0);
+            *count += 1;
+        }
+
+    frequencies
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_num_frequency() {
+        let numbers = vec![1, 2, 3, 4, 1, 2, 3, 1, 2, 1];
+        let map = frequency(numbers);
+        let result = map.get(&1).unwrap().clone();
+        assert_eq!(4, result)
+    }
+
+    #[test]
+    fn test_word_frequency() {
+        let sentence = "hello world wonderful world";
+        let map = frequency(sentence.split_whitespace().collect());
+        let result = map.get("world").unwrap().clone();
+        assert_eq!(2, result)
+    }
+}
--- hashmap-count/src/main.rs
+++ hashmap-count/src/main.rs
@@ -1,31 +1,26 @@
 /*
 This example code counts the frequency of each number in the vector.
  */
-use std::collections::HashMap;
-
-fn logic(numbers: Vec<i32>) -> Vec<(i32, u32)> {
-    let mut frequencies = HashMap::new();
-
-    for num in numbers {
-        let frequency = frequencies.entry(num).or_insert(0);
-        *frequency += 1;
-    }
-
-    let mut result = Vec::new();
-
-    for (num, frequency) in frequencies {
-        result.push((num, frequency));
-    }
-
-    result
-}
+use clap::Parser;
+use hashmap_count::{frequency, Config};
 
 fn main() {
-    let numbers = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 3];
-    let result = logic(numbers);
+    let args = Config::parse();
+    let numbers: Vec<i32> = args.num_list
+                                .unwrap()
+                                .split(",")
+                                .map(|n| n.parse::<i32>().unwrap())
+                                .collect();
+
+    //let numbers = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 3];
+    let map = frequency(numbers);
+    let mut result = Vec::new();
+    for (key, frequency) in map {
+        result.push(format!("{}: {}", key, frequency))
+    }
+
     //print the results in a human readable format that explains what the result is.
     println!(
-        "The frequency of each number in the vector is: {:?}",
-        result
-    );
+        "The frequency of each number in the vector is:\n{:?}",
+        result);
 }
